fe2399ff7c3f7c1eebddc0879da063f62d7457cf
anatofuz <anatofuz@gmail.com>
2025-01-27 09:12:37 +0900

    add config

--- zsh.nix
+++ zsh.nix
@@ -7,6 +7,50 @@
       syntaxHighlighting.enable = true;
       enableCompletion = true;
 
+      initExtra = ''
+        function peco-kill(){
+            for pid in `ps aux| peco --prompt "PROCESS >" |awk '{ print $2 }'`
+            do
+                kill $pid
+                echo "Killed $pid"
+            done
+        }
+        alias pk="peco-kill"
+        export dirfile="$HOME/.dirfile"
+
+        autoload -Uz add-zsh-hook
+        autoload -Uz chpwd_recent_dirs cdr
+        add-zsh-hook chpwd chpwd_recent_dirs
+
+        function save_dir_history {
+            echo $PWD >>| $dirfile
+        }
+
+        chpwd_functions+=save_dir_history
+
+        function hd {
+            cd $(cat $dirfile | tail -1)
+        }
+
+        function peco-z-search
+        {
+            which peco > /dev/null
+            if [ $? -ne 0 ]; then
+                echo "Please install peco "
+                return (1)
+            fi
+            local res=$(cat $dirfile | perl -CSD -ne 'BEGIN{%dir;} chomp($_); $dir{$_}++; END{map { print "$_\n" if (-d $_) } grep { $_ !~ /game/ } keys %dir}' | peco)
+            if [ -n "$res" ]; then
+                BUFFER+="cd $res"
+                zle accept-line
+                else
+                    return 1
+                fi
+         }
+        zle -N peco-z-search
+        bindkey '^[pz' peco-z-search
+      '';
+
       shellAliases = {
         gs = "git status";
         gc = "git commit";
